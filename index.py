4af13e04a33cb13988f13aa0c529275f67ea732a
Bre Thunholm <bthunholm@gmail.com>
2021-05-07 06:55:13 -1000

    new solutions

--- index.py
+++ index.py
@@ -36,3 +36,40 @@ print(str2)
 
 str2=f'[{name.capitalize()}] loves the color {color.lower()}!'
 print(str2)
+
+
+# User Input
+
+name= input('What is your name?:')
+age=input('What is your age?:')
+print('Good Morning ' + name + '! You are '+ age +' years old.')
+
+
+# Calculator 
+
+num1= input('Enter a digit: ')
+num2= input('Enter a second number:')
+answer= float(num1)+float(num2)
+print(answer)
+
+# User Input - Exercise
+# Distance converter km to miles 
+
+name=input('Please enter your name:')
+km =input('Enter the number of kilometers you wish to convert to miles: ')
+miles= float(km)/1.609
+print(f'Hi {name.title()}! {km}km is equivalent to  {round(miles, 1)} miles')
+
+# Lists
+groceries=['eggs','milk','cheese','lettuce']
+
+
+# find length len(groceries)
+# index of specific item groceries.index('item')
+
+groceries.sort(reverse=True)
+
+
+nums= [1,2,6,8,24,57,33]
+print(min(nums))
+print(max(nums))
