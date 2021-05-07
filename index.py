27f87b109911c3aa596364dcf664ae1dd6e8eaf7
Bre Thunholm <bthunholm@gmail.com>
2021-05-06 17:55:06 -1000

    python string reverse and variables

---
+++ index.py
@@ -0,0 +1,17 @@
+
+
+#slicing strings\
+
+msg ='Bre is cool'
+print(msg[0])
+print(msg[2:10])
+
+# String exercise 
+
+str = 'Welcome to Python 101: Strings'
+str1=str[18]+' '+str[:8]+str[25:29]+str[7:11]+str[8]+str[12]+str[2]+str[1]+str[-5]
+
+
+
+print(str1.title())
+print(str1[::-1].title())
