a85f73f65dff660ad28ff054cb9cd7858270625a
Bre Thunholm <bthunholm@gmail.com>
2021-05-06 18:39:04 -1000

    new solutions

--- index.py
+++ index.py
@@ -3,8 +3,8 @@
 #slicing strings\
 
 msg ='Bre is cool'
-print(msg[0])
-print(msg[2:10])
+# print(msg[0])
+# print(msg[2:10])
 
 # String exercise 
 
@@ -15,3 +15,24 @@ str1=str[18]+' '+str[:8]+str[25:29]+str[7:11]+str[8]+str[12]+str[2]+str[1]+str[-
 
 print(str1.title())
 print(str1[::-1].title())
+
+# Multiline Strings
+sentence="""Hi my name is
+Bre and I love Luke so much. And
+also Archie!"""
+
+print(sentence)
+print(sentence.find('Bre'))
+print(msg.replace('Bre', 'Luke'))
+
+
+print('Bre' not in msg)
+
+## String formatting 
+name='bre'
+color='PINK'
+str2= '['+name+'] loves the color ' + color + '!'
+print(str2)
+
+str2=f'[{name.capitalize()}] loves the color {color.lower()}!'
+print(str2)
